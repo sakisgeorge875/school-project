34c94e6b150b3fa66fa75f72c14ed0355258c91a
sakisgeorge875 <sakisgeorge875@gmail.com>
2025-03-22 14:48:47 +0000

    Add 16.c

---
+++ src/16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int i;
+
+    printf("Enter an integer: ");
+    scanf("%d", &i);
+
+    if (i % 2 == 0) {
+        printf("%d is even\n", i);
+    } else {
+        printf("%d is odd\n", i);
+    }
+}
